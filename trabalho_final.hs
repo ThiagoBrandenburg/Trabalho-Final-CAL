b3f7fd44ebb56bc00c3d162c67ef1e6678150143
Thiago Brandenburg <thiagobranden@hotmail.com>
2022-06-15 18:02:37 -0300

    update 1.1
    Começando a implementacao de MR teste de primalidade

--- trabalho_final.hs
+++ trabalho_final.hs
@@ -6,8 +6,22 @@ myPureFunction x = x*x
 rand_provisorio :: Int
 rand_provisorio = fst (next (mkStdGen 1))
 
-drawInt :: Integer -> Integer -> IO Integer
-drawInt x y = getStdRandom (randomR (x,y))
+randBigIntRange :: Integer -> Integer -> IO Integer
+randBigIntRange x y = getStdRandom (randomR (x,y))
+
+fatoracao :: Integer -> Integer
+fatoraca
+        
+
+m_r_primalidade :: Integer -> Int -> Bool
+m_r_primalidade 1 k =
+    False
+m_r_primalidade 2 k =
+    True
+m_r_primalidade n 0 =
+    True
+mr_primalidade n k =
+    
 
 
 
